841d31587433b3ae760e3971867e21076e9c645b
faizanejaz14 <faizanejaz14@gmail.com>
2026-04-10 09:25:37 +0500

    checking rule set from contribution1 branch

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,5 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+  now adding something from contribution1 branch to see rule set on main brnach and creating a PR
